b94fd78f85acccb770328e3fefa40bb41dc05d52
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-05-15 23:37:36 +0000

    Lecture 1

---
+++ js_basics/lecture1.js
@@ -0,0 +1,34 @@
+const accountId = 14453
+
+let accountEmail = "abc@gmail.com"
+
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+
+
+let accountState;
+
+
+
+//accountId = 2
+
+accountEmail = "abc2@gmail.com"
+
+accountPassword = "223"
+
+accountCity = "BWN"
+
+
+console.log(accountId);
+
+// Log Shortcut
+
+// step 2
+console.table({accountEmail,accountPassword,accountCity,accountState})
+ 
+/*
+Multiline Comment 
+Preffered not to use Var.
+*/
+
