52c56fbf0a8a5c3c47cb8fad5915ad493be3cf19
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-05-16 22:59:16 +0000

    dtypes

---
+++ js_basics/dtypes.js
@@ -0,0 +1,35 @@
+"use strict"; // To Use New version Javascript version
+
+
+console.log("Data Types")
+
+// alert(3 +3)  used in html not in Node.
+
+// console.log("Line 1"); console.log("Line 2 part 2");
+
+// It eleminated code readability
+
+console.log("Line 1")
+console.log("Line 2 part 2")
+// this is a good example
+
+// Ensure code readability.
+
+/*
+string
+number
+boolean
+bigint
+symbol unique
+null standalone
+undefined
+
+
+//object
+
+*/
+
+
+let thisvar = null
+
+console.log(typeof thisvar)
