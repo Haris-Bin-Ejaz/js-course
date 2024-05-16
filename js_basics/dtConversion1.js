f28b05246e1f455848b212fb5e86095d3e8a6169
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-05-16 23:16:07 +0000

    Data Type Conversion 1

---
+++ js_basics/dtConversion1.js
@@ -0,0 +1,43 @@
+// Dtypes Conversion
+
+/*
+let score = 33
+
+let score2 = null
+
+let stringScore = "33a"
+
+console.log(typeof score);
+
+console.log(typeof (stringScore));
+
+let valueInNumber = Number(stringScore)
+
+
+let value2InNumber = Number(score2)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)
+
+console.log(value2InNumber)
+*/
+
+// let isLogged = 1
+
+// let isLogged2 = "Haris"
+
+// let booleanIsLogged  = Boolean(isLogged2)
+
+// console.log(booleanIsLogged)
+
+/*Notes
+"33" => 33
+n33 => Nan
+true => 1 && false => 0 and vice versa
+"" => False
+"Haris"  => True
+33 => "33"
+
+*/
+
