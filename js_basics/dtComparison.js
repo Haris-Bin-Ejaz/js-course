fe9373fc58dc8f9ed89c8fd4b38c46c99c502f87
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-05-16 23:41:31 +0000

    Data Types Comparison

---
+++ js_basics/dtComparison.js
@@ -0,0 +1,20 @@
+// console.log( 2> 1);
+// console.log( 2 ==1);
+// etc
+
+// console.log("2" > 1);
+
+// console.log(null > 0);
+
+// console.log(0 == null)
+
+// console.log( null >= 0);
+
+
+// console.log(undefined == 0);
+
+// Avoid using these comparisons
+
+// strict check ===
+
+console.log("2" === 2);
