0277d25b2171e0157f9bc156c3b0c13574b29f9f
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-05-17 00:00:24 +0000

    Data Type Summary

---
+++ js_basics/dtsummary.js
@@ -0,0 +1,69 @@
+// Categories on the basis of
+
+// How to Use
+// How to Access
+
+// 1. Premitive (Call by Value)
+
+// -7 Types
+/*
+String
+let v1 = "String1"
+
+Number
+let v2 = 1
+let v3 = 1.0
+
+Boolean
+let isLogged = true
+
+Null
+let v4 = null
+
+Undefined
+let v5 = undefined
+
+Symbol
+const id = Symbol('123')
+
+const id2 = Symbol = ('123')
+
+BigInt
+
+let v6 = 5484484844684n;
+
+
+*/ 
+
+
+// 2. Reference Type (Non Premitive)
+
+/*
+
+Arrays
+
+
+Objects
+Functions
+
+*/
+
+const heros = ["Shaktiman", 
+"Krish"]  // array
+
+let stdData = {
+    name: "Haris",
+    age: 22,
+} // object
+
+
+const myFuncVar = function(){""} // function in var
+
+// typeof function
+
+
+
+
+
+// JS is Dynamically Types
+
