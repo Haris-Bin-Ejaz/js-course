68ddffb81ca2fc7789f7269929973768d6083291
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-06-23 13:05:22 +0000

    Numbers and Math

---
+++ js_basics/numbers.js
@@ -0,0 +1,57 @@
+console.log("Numbers")
+
+const balance = new Number(100)
+
+// console.log("Number is " + balance);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2)); // Fixed Point after point
+
+const otherNum = 123.8966
+
+// console.log(otherNum.toPrecision(3)); // round number
+
+
+const hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString());
+
+// console.log(hundreds.toLocaleString('UR-PK'));// Currency Format
+
+
+
+console.log(Math);
+
+
+console.log(Math.abs(-4)); // coverts - to +
+
+console.log(Math.round(5.5)); // round simple
+
+console.log(Math.ceil(4.2)); // always next value
+
+console.log(Math.floor(4.9)); // always previous value
+
+
+console.log(Math.min(1,5,6));
+
+console.log(Math.max(1,4,55));
+
+console.log('Random Values');
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min
+)
+
+
+
+
