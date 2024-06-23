0a78be6e3164caec52eb7730f4071f9c2f8998a9
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-06-23 18:07:52 +0000

    destructuring,api json format

---
+++ js_basics/object3.js
@@ -0,0 +1,35 @@
+console.log("Object in Depth 3 Apis\n");
+
+const course = {
+
+    name : "Java Script",
+    price : 999,
+    couseInstructor : "Hitesh"
+    
+}
+
+// course.couseInstructor
+// Destructuring Objects 
+
+
+//const {couseInstructor: instructor} = course
+
+//console.log(instructor);
+
+
+// //json format
+// {
+
+//     "name" : "abc",
+//     "courseName" : "javascript",
+//     "price" : "free"
+
+// }
+
+// api in array
+
+// [
+// {},
+// {},
+// {}
+// ]
