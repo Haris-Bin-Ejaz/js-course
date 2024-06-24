d8ea7feda0bc9a960772e3b66f1f92d958e6e25e
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-06-24 19:06:18 +0000

    scope in depth

--- jfunctions/scope.js
+++ jfunctions/scope.js
@@ -1,21 +1,65 @@
 console.log("Js Scope");
 
-let a = 10;
-const b = 110;
-var c = 30;
+// let a = 10;
+// const b = 110;
+// var c = 30;
 
-if(true){
+// if(true){
 
-var c = 40
-//c = 20
+// var c = 40
+// //c = 20
 
-let b = 20;
+// let b = 20;
+// }
+
+
+
+// console.log(c);
+// console.log(b);
+
+
+// function one(){
+//     const username = "Haris"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     //console.log(website);
+
+//    // two()
+// }
+
+// one()
+
+// if(true){
+//     username2 = "Haris"
+//     if (username2 === "Haris"){
+//         const website = "Youtube"
+
+//         console.log(username2+ website);
+//     }
+//     //console.log(website);
+// }
+
+// //console.log(username2);
+
+// ...................interesting........................
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
 }
 
+//addOne(5)
 
+//console.log();
+ 
+//addTwo(5) error
 
-console.log(c);
-console.log(b);
-
+const addTwo = function(num){
+    return num + 2
+}
 
 
