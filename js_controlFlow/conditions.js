3beab1e0b969bfd88936cbfc821137bd326650da
Haris-Bin-Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-06-25 13:25:22 +0000

    Conditions in Javascript

---
+++ js_controlFlow/conditions.js
@@ -0,0 +1,16 @@
+console.log("Conditionals Statements\n");
+
+//if (true){}
+
+//if (false){}
+
+const isUserLoggedIn = true
+if(isUserLoggedIn){console.log("UserLoggedIn");}
+
+const result = 3 != 3
+const result2 = 3 === "3"
+
+console.log(result);
+console.log(result2);
+
+
