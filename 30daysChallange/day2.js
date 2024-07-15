6bd741a531ff5f2b29dc7fa47047e04015b415d0
Haris Bin Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-07-15 20:39:26 +0000

    day 2 Operators

---
+++ 30daysChallange/day2.js
@@ -0,0 +1,78 @@
+//Activity 1
+
+let A = 55
+let B = 57
+let sum = A + B
+let subtract = sum - 10
+let multiply = sum * 2
+let divide = sum/2
+let remainder = sum%3
+
+// console.log("Sum = ",sum , "\n");
+// console.log("Difference = ",subtract , "\n");
+// console.log("Product = ",multiply , "\n");
+// console.log("Divide result = ",divide , "\n");
+// console.log("Remainder = ",remainder , "\n");
+
+
+// console.log('------------------------');
+
+//Activity 2
+
+let incrementedSum  = sum += 100
+let decrementedSum = sum -= 100
+
+// console.log(
+//     `     Incremented Sum = ${incrementedSum} \n
+//      Decremented Sum ${decrementedSum}`
+// );
+
+//-----------------------------------------------------
+
+// Activity 3
+
+// console.log(`A Greater than B : ${A > B }`);
+
+// console.log(`A Greater than or Equal to B : ${A >= B }`);
+
+// ;
+
+// console.log(`A Smaller than B : ${A < B }`);
+
+// console.log(`A Smaller than or Equal to B : ${A <= B }`);
+
+// ;
+
+// -----------------------------
+
+let A2 = `${A}`
+
+// console.log(`A == equal to A2 ${A == A2}`);
+// console.log(`A  === equal to A2 ${A === A2}`);
+
+
+//-----------------------------------------
+
+
+// Activity 4
+
+let orCase = A == A2 || A > B
+let andCase = A === A2 && A > B
+
+// console.log('OR CASE: ',orCase);
+// console.log('AND CASE: ',andCase);
+// console.log('NOR OR CASE: ',!orCase);
+// console.log('NOR And CASE: ',!andCase);
+
+//-----------------------------------
+
+// Activity 5
+
+let guessnum = -1
+
+let positiveOrNegative = guessnum > 0 ? "Number is Positive" : "Number is Negative"
+
+console.log(`The Number: ${guessnum} is ${positiveOrNegative}`);
+
+
+
