17ea264175025f060e0a06c346106def22331003
Haris Bin Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-07-15 20:06:10 +0000

    day 1

---
+++ 30daysChallange/day1.js
@@ -0,0 +1,83 @@
+var n1  = 10;
+//console.log(n1);
+
+var s1 = "Hellow World"
+//console.log(s1);
+
+const b1 = true
+//console.log(b1);
+
+
+// --------------------
+
+let num1 = 1
+let fnum = 3.14
+let string1 = "Hello"
+let arr1 = [1,2,3]
+let obj1 = {
+    name : 'obj1',
+    id : 1
+}
+let sym1 = Symbol('symbol1')
+
+let und = undefined;
+
+let nul = null;
+
+let bign = BigInt(12345n)
+
+
+// console.log(
+//     `\t Number = ${num1}\n  
+//      Float  Number = ${fnum} \n 
+//      String = ${string1} \n 
+//      Array = ${arr1} \n 
+//      Object = ${obj1} \n 
+//      Undefined = ${und} \n 
+//      Null = ${nul} \n 
+//      BigInt = ${bign}
+    
+//     `)
+
+//     console.log("---------------------------------\n");
+
+//console.log(sym1);
+
+// console.log(
+//     `\t Type of Number = ${typeof(num1)}\n  
+//          Type of Float  Number = ${typeof(fnum)} \n 
+//          Type of String = ${typeof(string1)} \n 
+//          Type of Array = ${typeof(arr1)} \n 
+//          Object = ${typeof(obj1)} \n 
+//          Undefined = ${typeof(und)} \n 
+//          Null = ${typeof(nul)} \n 
+//          BigInt = ${typeof(bign)}
+    
+//     `)
+
+//------------------------------------
+
+//Part 3
+
+let anyvar = 1
+
+// console.log(`Ànyvar before = ${anyvar}`);
+
+// anyvar = 2
+// console.log(`Ànyvar after = ${anyvar}`);
+
+
+// part 4
+
+const anyConst = 1
+
+//anyConst = 2
+
+//console.log(anyConst);
+// TypeError: Assignment to constant variable
+
+//console.log(typeof(sym1)); 
+
+//console.log(arr1 instanceof Object); true
+
+
