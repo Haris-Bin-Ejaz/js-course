cab0eb8aa9d0edcc195fd7a59c2be52817dada72
Haris Bin Ejaz <126745531+Haris-Bin-Ejaz@users.noreply.github.com>
2024-07-16 20:48:21 +0000

    Day 4 | Loops

---
+++ 30daysChallange/day4.js
@@ -0,0 +1,107 @@
+// Day 5 Loops
+
+// Activity 1
+
+// for (let i = 0; i < 10; i++ ) {
+//     console.log(i);   
+// }
+
+// for (let i = 1; i <= 10; i++ ) {
+//     console.log(`5 X ${i} = ${5*i}`);   
+// }
+
+//-----------------------------
+//Activity 2
+// let i = 0
+// let sum = 0
+// while(i <= 10){
+
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum);
+
+// let i = 10
+
+// while (i >= 0){
+
+//     console.log(i);
+//     i--
+// }
+
+//----------------------------------------------
+
+//Activity 3
+// let i = 1
+
+// do{
+
+//     console.log(i);
+//     i++
+// }while(i<= 5)
+
+
+// let i = 1
+// let num = 6
+// let display = ''
+// let result = 1
+// do{
+
+//     if (num == 0)console.log(display + 0);
+//     else if (num == 1)console.log(display + 1);
+//     else{
+//     result = result * i
+    
+
+//     if (i < num){display = display + i +"+" }
+//     else{display = display + i}
+    
+//     }
+    
+//     i++
+// }
+// while(i<= num)
+// console.log(`${display} = ${result}`);
+
+
+//-------------------------------------------
+
+//Activty 4
+
+// for (i = 0; i <= 5; i++) {
+     
+//     console.log("*".repeat(i));
+// }
+
+// let num = 6
+// for (i = 0; i <= num; i ++) {
+//     let row = ''
+//     for(j = 0; j < i; j++){
+    
+//         row += "*"
+//     }
+//     console.log(row);
+    
+// }
+
+//--------------------------------------------
+
+//Activity 5
+
+// for(let i =0; i <= 10; i ++ ){
+
+//     if(i == 5) continue
+
+//     console.log(i);
+// }
+
+
+// for(let i =0; i <= 10; i ++ ){
+
+//     if(i == 7) break
+
+//     console.log(i);
+// }
+
+
+
