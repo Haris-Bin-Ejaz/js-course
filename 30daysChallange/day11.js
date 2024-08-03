f28c94cb21be3fc49fc8d94c24c35da6cc811823
Haris Bin Ejaz <Harisijaz482@gmail.com>
2024-08-03 15:12:52 +0500

    Day 11 | Promises

--- 30daysChallange/day11.js
+++ 30daysChallange/day11.js
@@ -38,61 +38,206 @@
 //----------------------------------------------------------------------------
 // Activity 2 
 
-let data = 0
+// let data = 0
 
-let fetchdata1 = new Promise(
-    (resolve,reject)=>{
+// let fetchdata1 = new Promise(
+//     (resolve,reject)=>{
+//         setTimeout(()=>{
+//             data += 1
+//             console.log("Data from Server");
+//             resolve(`Data is added ${data}`)
+//         },1000)
+//     }
+// )
+// let fetchdata2 = new Promise(
+//     (resolve,reject)=>{
+//         setTimeout(()=>{
+//             data += 1
+//             console.log("Data from Server");
+//             resolve(`Data is added ${data}`)
+//         },1000)
+//     }
+// )
+// let fetchdata3 = new Promise(
+//     (resolve,reject)=>{
+//         setTimeout(()=>{
+//             data += 1
+//             console.log("Data from Server");
+//             resolve(`Data is added ${data}`)
+//         },1000)
+//     }
+// )
+
+
+
+// fetchdata1
+// .then(
+//     (data1)=> {
+//         console.log(data1);
+//         return fetchdata2
+//     }
+// )
+// .then(
+//     (data2)=> {
+//         console.log(data2);
+//         return fetchdata3
+//     }
+// )
+// .then(
+//     (data3)=> {
+//         console.log(data3);
+//         console.log("Data Addition Complete");
+//     }
+// )
+// .catch(
+//     ()=>{
+//         console.log("Data addition failed");
+//     }
+// )
+
+
+//----------------------------------------------------------------------------
+// Activity 3
+
+// async function getResolvedValue(){
+//     new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve("Promised Resolved")
+
+//         },1000)
+//     }).then((resolveValue)=>{
+//         console.log(resolveValue);
+//     })
+
+    
+// }
+
+//getResolvedValue()
+
+//....................
+
+// async function getResolvedValue(){
+//     let promise = new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve("Promised Resolved")
+
+//         },1000)
+//     })
+
+//     //console.log(await promise);
+
+//     console.log(promise);
+
+
+
+    
+// }
+
+// getResolvedValue()
+
+//.................................................
+
+
+async function getRejectedValue(){
+
+    let promise = new Promise((_r, reject)=>{
+
         setTimeout(()=>{
-            data += 1
-            console.log("Data from Server");
-            resolve(`Data is added ${data}`)
+
+            reject('Promise Rejected')
+
         },1000)
+
+    })
+
+    try{
+        
+        console.log(await promise);
     }
-)
-let fetchdata2 = new Promise(
-    (resolve,reject)=>{
-        setTimeout(()=>{
-            data += 1
-            console.log("Data from Server");
-            resolve(`Data is added ${data}`)
-        },1000)
+
+    catch(error){
+
+        console.log(error);
+
     }
-)
-let fetchdata3 = new Promise(
-    (resolve,reject)=>{
-        setTimeout(()=>{
-            data += 1
-            console.log("Data from Server");
-            resolve(`Data is added ${data}`)
-        },1000)
-    }
-)
+}
+
+//getRejectedValue()
+
+//----------------------------------------------------------------------------
+// Activity 4
+
+// let promiseData1 = new Promise((resolve, reject)=>{
+    
+//     let data = fetch("https://randomuser.me/api/")
+//     resolve(data)
+
+// }).then((data)=>{
+
+//     return data.json()
+
+// }).then((data)=>{
+
+//     console.log(data);
+    
+// })
+
+
+//...........................
+
+//using async Await
+
+// async function getData() {
+    
+//     try {
+
+//         let data = await fetch("https://randomuser.me/api/")
+//         resolve(data)
+        
+//     } catch (error) {
+//         console.log("Failed to get data");
+        
+//     }
+    
+// }
+
+// getData()
 
 
 
-fetchdata1
-.then(
-    (data1)=> {
-        console.log(data1);
-        return fetchdata2
-    }
-)
-.then(
-    (data2)=> {
-        console.log(data2);
-        return fetchdata3
-    }
-)
-.then(
-    (data3)=> {
-        console.log(data3);
-        console.log("Data Addition Complete");
-    }
-)
-.catch(
-    ()=>{
-        console.log("Data addition failed");
-    }
-)
+//.....................................................................
+//Activity 5
 
+let p1 = Promise.resolve("Direct Value")
+//let p2 = Promise.reject("Direct Value P2")
+
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'Value in timeout');
+  });
+
+// let p3 = new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+
+//         resolve("Value in Resolve")
+
+//     },100);
+//   });
+
+let p4 = 10
+  
+Promise.all([p1,p3,p4]).then((values)=>{
+    console.log(values);
+    
+}).catch((error)=>{
+    console.log(error);
+    
+})
+
+Promise.race([p1,p3,p4]).then((values)=>{
+    console.log(values);
+    
+}).catch((error)=>{
+    console.log(error);
+    
+})
 
